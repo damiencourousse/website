3772a5300be3d806eba5954ba400eb2deb854ac9
Damien Couroussé <damien.courousse@gmail.com>
2014-11-22 00:39:25 +0100

    BibParse: generalise the use of links in the note bibtex field
    Allow for all sorts of pairs of values (link, url) in bibtex entries, in
    'note' fields, as follows :
    @misc{myentry,
        note = { paper = /link/paper.pdf,
    	     slides = other-path-to-slides.pdf,
    	     webslides = http://my.slides.com,
    	     some legacy bibtex notes
    	   }

--- src/BibParse.hs
+++ src/BibParse.hs
@@ -4,6 +4,7 @@ module BibParse
     )
     where
 
+import Data.String.Utils (strip)
 import Control.Applicative ((<*))
 import Data.List (intersperse)
 import Text.Parsec
@@ -28,19 +29,18 @@ test = do
 
 data KeyData = KeyData { key :: Maybe String   -- the original key string
                        , keyId :: Maybe String -- the key id of the citation to process
-                       , paper  :: Maybe String --
-                       , slides :: Maybe String --
-                       , poster :: Maybe String
                        , notes :: Maybe String
+                       , links :: [(Ltext, Url)]
                        } deriving (Show)
+
+newtype Ltext = Ltext String  deriving (Show)
+newtype Url = Url String  deriving (Show)
 
 emptyKeyData :: KeyData
 emptyKeyData = KeyData { key = Nothing
                        , keyId = Nothing
-                       , paper = Nothing
-                       , slides = Nothing
-                       , poster = Nothing
                        , notes = Nothing
+                       , links = []
                        }
 
 processCitation :: [Reference] -- ^ the database of biblio references
@@ -57,7 +57,7 @@ processCitation refs k = genCiteMetadata $ union [k, keydata]
           citeKey = emptyCite { citeId = fromMaybe "malformed KeyData" $ keyId k }
 
 genCiteMetadata :: KeyData -> String
-genCiteMetadata k  = unwords [ fromMaybe "key: empty" $ key k
+genCiteMetadata k  = unwords $ [ fromMaybe "key: empty" $ key k
                              {-, fromMaybe "keyId: empty\n" $ keyId k-}
                              --
                              -- Markdown links always start with the link text
@@ -69,27 +69,16 @@ genCiteMetadata k  = unwords [ fromMaybe "key: empty" $ key k
                              -- @smith04 [p. 33] says blah.
                              -- refs: http://johnmacfarlane.net/pandoc/README.html#citations
                              , "<!-- here follow the markdown metadata -->"
-
-                             -- TODO  how to generate links for other formats?
-                             , genPaper $ paper k
-                             , genSlides $ slides k
-                             , genPoster $ poster k
                              ]
+                             ++ (map genHrefs $ links k)
 
-genPaper :: Maybe String -> String
-genPaper (Just k) = "[Pdf](" ++ k ++ ") "
-genPaper Nothing  = ""
-
-genSlides :: Maybe String -> String
-genSlides (Just k) = "[Slides](" ++ k ++ ") "
-genSlides Nothing  = ""
-
-genPoster :: Maybe String -> String
-genPoster (Just k) = "[Poster](" ++ k ++ ") "
-genPoster Nothing  = ""
+genHrefs :: (Ltext, Url) -> String
+genHrefs (Ltext h, Url u) = "[" ++ h ++ "](" ++ u ++ ") "
 
 
-
+-- |
+-- >>> parse parseBibtexNote   "" "paper=/media/paper_0.pdf, poster = /poster.pdf"
+-- Right (KeyData {key = Nothing, keyId = Nothing, notes = Nothing, links = [(Ltext "paper",Url "/media/paper_0.pdf"),(Ltext "poster",Url "/poster.pdf")]})
 parseBibtexNote :: Parser KeyData
 parseBibtexNote = do
     elems <- parseNoteElement `sepBy` (char ',')
@@ -101,54 +90,37 @@ union :: [KeyData] -> KeyData
 union ks = foldr step emptyKeyData ks
   where
     step :: KeyData -> KeyData -> KeyData
-    step (KeyData a b c d e f) (KeyData a' b' c' d' e' f') =
+    step (KeyData a b n xs) (KeyData a' b' n' xs') =
         KeyData (mplus a a')
                 (mplus b b')
-                (mplus c c')
-                (mplus d d')
-                (mplus e e')
-                (mplus f f')
+                (mplus n n')
+                (xs ++ xs')
 
 parseNoteElement :: Parser KeyData
-parseNoteElement = try parsePaper
-                 <|> try parseSlides
-                 <|> try parsePoster
-                 <|> try parseOtherNotes
+parseNoteElement =   try parseLink
+                 <|> try parseNotes
 
-parseElem :: String             -- ^ the element identifier
-          -> Parser String
-parseElem e = do
+parseLink :: Parser KeyData
+parseLink  = do
     spaces
-    _ <- string e <* spaces
+    href <- many1 $ noneOf "=" <* spaces
     _ <- string "=" <* spaces
-    many1 $ noneOf ","
+    url <- many1 $ noneOf ","
+    return $ emptyKeyData { links = [(Ltext $ strip href, Url $ strip url)] }
 
 -- |
--- >>> parse parsePaper  "" "paper=/media/paper_0.pdf"
--- Right (KeyData {key = Nothing, keyId = Nothing, paper = Just "/media/paper_0.pdf", slides = Nothing, poster = Nothing, notes = Nothing})
-parsePaper :: Parser KeyData
-parsePaper = do
-    liftM (\x -> emptyKeyData { paper = Just x }) $
-        parseElem "paper"
+-- >>> parse parseLink  "" "paper=/media/paper_0.pdf"
+-- Right (KeyData {key = Nothing, keyId = Nothing, notes = Nothing, links = [(Ltext "paper",Url "/media/paper_0.pdf")]})
+-- >>> parse parseLink  "" " paper=/media/paper_0.pdf "
+-- Right (KeyData {key = Nothing, keyId = Nothing, notes = Nothing, links = [(Ltext "paper",Url "/media/paper_0.pdf")]})
 
-parseSlides :: Parser KeyData
-parseSlides = do
-    liftM (\x -> emptyKeyData { slides = Just x }) $
-        parseElem "slides"
-
-parsePoster :: Parser KeyData
-parsePoster = do
-    liftM (\x -> emptyKeyData { poster = Just x }) $
-        parseElem "poster"
-
-parseOtherNotes :: Parser KeyData
-parseOtherNotes = do
+parseNotes :: Parser KeyData
+parseNotes = do
     liftM (\x -> emptyKeyData { notes = Just x }) $
         many1 $ noneOf ","
 
-
-parse' :: Parser String -> String -> String
-parse' p txt = fromEither $ parse (p <* eof) "" txt
+parseStr :: Parser String -> String -> String
+parseStr p txt = fromEither $ parse (p <* eof) "" txt
 
 fromEither :: Either ParseError String -> String
 fromEither e = case e of
@@ -157,7 +129,7 @@ fromEither e = case e of
 
 
 processCitations :: [Reference] -> String -> String
-processCitations refs body = parse' (parseCitations $ processCitation refs) body
+processCitations refs body = parseStr (parseCitations $ processCitation refs) body
 
 -- |
 -- parse a page body, extract bibliographic citations,
