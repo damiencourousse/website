4c82fb928d515593e027a99a83e83c05ce0ebf62
Damien Couroussé <damien.courousse@posteo.net>
2019-08-07 21:25:14 +0200

    add support for an image gallery: 'expressions urbaines'

--- src/site.hs
+++ src/site.hs
@@ -1,10 +1,21 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
 
-import           Data.Maybe      (fromMaybe)
-import           Data.Monoid     ((<>))
-import qualified Data.Text       as T
+import           Control.Applicative           (Alternative (..))
+import           Control.Monad                 (liftM)
+import           Data.List                     (sortBy)
+import           Data.Maybe                    (fromMaybe)
+import           Data.Monoid                   ((<>))
+import           Data.Ord                      (comparing)
+import qualified Data.Text                     as T
 import           Hakyll
-import           System.Process  (readProcess)
+import           Hakyll.Core.Compiler.Internal
+import           System.Directory              (createDirectoryIfMissing,
+                                                doesFileExist)
+import           System.FilePath
+import qualified System.Process                as Process
 import           Text.Pandoc
 
 import           BibParse
@@ -64,6 +75,57 @@ main = do
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
 
+
+    -- the photo gallery -------------------------
+
+    -- copy original images
+    let expUImages :: Pattern
+        expUImages =      "images/expressions-urbaines/*.jepg"
+                     .||. "images/expressions-urbaines/*.jpg"
+                     .||. "images/expressions-urbaines/*.JPG"
+                     .||. "images/expressions-urbaines/*.png"
+
+    match expUImages $ do
+      route $ gsubRoute "expressions-urbaines/" (const "")
+      compile copyFileCompiler
+
+    -- create thumbnails
+    match expUImages $ version "small" $ do
+      route $
+        gsubRoute "expressions-urbaines/" (const "") `composeRoutes` appendFileRoute "small"
+      compile $ resizeImage 600 600
+
+    -- the gallery page
+    create ["expressions-urbaines.html"] $ do
+      route idRoute
+      compile $ do
+        let imageCtx :: Context a
+            imageCtx = urlField "url"
+                       <> basenameField "id"
+                       <> pathField "path"
+                       <> mdateFile "mdate"
+                       <> imageAlt "alt"
+
+            photosCtx :: Context String
+            photosCtx = listField "photos" (imageCtx :: Context CopyFile)
+                        (reverse <$> (loadAll ("images/expressions-urbaines/*" .&&. hasNoVersion)
+                                      >>= sortByMdate))
+                        <> listField "photos-small" (imageCtx :: Context ())
+                           (reverse <$> (loadAll ("images/expressions-urbaines/*" .&&. hasVersion "small")
+                                         >>= sortByMdate))
+                        <> defaultContext
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/photos.html" photosCtx
+          >>= loadAndApplyTemplate "templates/default.html"
+              -- the git date info is extracted for the directory
+              -- containing the input images
+              (gitCommitDateWith (const "images/expressions-urbaines")
+               <> constField "title" "expressions urbaines"
+               <> builtPageCtx)
+          >>= relativizeUrls
+      -- MAYBE  resize images so that they load faster.
+
+    -- templates
     match "templates/*" $ compile templateCompiler
 
     -- robots, sitemap, etc.
@@ -74,12 +136,9 @@ main = do
     create ["sitemap.xml"] $ do
         route idRoute
         compile $ do
-            -- posts <- recentFirst =<< loadAll "posts/*"
-            -- pages <- loadAll "pages/*"
-            -- let allPages = pages ++ posts
-            let sitemapCtx = builtPageCtx <> lastGitModification
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
+          let sitemapCtx = builtPageCtx <> gitCommitDate
+          makeItem ""
+            >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
 
 feedConfiguration :: FeedConfiguration
 feedConfiguration = FeedConfiguration
@@ -90,7 +149,9 @@ feedConfiguration = FeedConfiguration
                   , feedRoot = "http://damien.courousse.fr"
                   }
 
--- Auxiliary compilers
+-- * Auxiliary compilers
+--------------------------------------------------------------------------------
+
 --    the main page compiler
 publiCompiler :: Compiler (Item String)
 publiCompiler = do
@@ -155,14 +216,14 @@ appendCitations :: Item Biblio -> Item String -> Compiler (Item String)
 appendCitations bib t = pure (processCitations refs <$> t)
     where Biblio refs = itemBody bib
 
--- Context builders
+-- * Context builders
+--------------------------------------------------------------------------------
 builtPageCtx :: Context String
 builtPageCtx =  constField "siteroot" (feedRoot feedConfiguration)
              <> listField "entries" builtPageCtx (loadAll $ "pages/*" .||. "posts/*")
              <> dateField "date" "%A, %e %B %Y"
              <> dateField "isodate" "%F"
-             <> gitTag
-             <> lastGitModification
+             <> gitCommitDate
              <> defaultContext
 
 postCtx :: Context String
@@ -171,27 +232,134 @@ postCtx =  dateField "date" "%B %e, %Y"
         <> modificationTimeField "mtime" "%F"
         <> defaultContext
 
--- | Extracts git commit info and render some html code for the page footer.
+
+-- | Extracts git commit info
 --
 -- Adapted from
 -- - Jorge.Israel.Peña at https://github.com/blaenk/blaenk.github.io
 -- - Miikka Koskinen at http://vapaus.org/text/hakyll-configuration.html
-gitTag :: Context String
-gitTag = field "gitinfo" $ \item -> do
-  let fp = toFilePath $ itemIdentifier item
-      gitLog format =
-        readProcess "git" ["log", "-1", "HEAD", "--pretty=format:" ++ format, fp] ""
-  unsafeCompiler $ do
-    date    <- gitLog "%aD"
-    return $ concat
-             [ "<a href=https://github.com/damiencourousse/damiencourousse.github.io/commits/master>"
-             , "Page last modified " ++ date
-             , "</a>"
-             ]
+gitLogWith
+  :: (Item a -> FilePath)
+  -> String -- ^ the @Context@ key
+  -> String  -- ^ format argument to option @--pretty@ of @git log@
+  -> Context a
+gitLogWith f key format = field key $
+  \item -> unsafeCompiler $
+    Process.readProcess "git" ["log", "-1", "HEAD", "--pretty=format:" ++ format, f item] ""
 
--- | Extract the last modification date from the git commits
-lastGitModification :: Context a
-lastGitModification = field "lastgitmod" $ \item -> do
+-- | Extract the git commit date of the file sourcing the targeted Item (context field: @lastgitmod@).
+gitCommitDate :: Context a
+gitCommitDate = gitLogWith (toFilePath . itemIdentifier) "lastgitmod" "%aD"
+
+-- | Extract the git commit date of the target file (context field: @lastgitmod@).
+gitCommitDateWith :: (Item a -> FilePath) -> Context a
+gitCommitDateWith f = gitLogWith f "lastgitmod" "%aD"
+
+-- | last modification date of the file targeted by an item
+mdateFile
+  :: String -- ^ the context key
+  -> Context a
+mdateFile key = field key $ \item -> unsafeCompiler $ do
   let fp = toFilePath $ itemIdentifier item
-  unsafeCompiler $
-    readProcess "git" ["log", "-1", "HEAD", "--pretty=format:%ad", "--date=short", fp] ""
+  Process.readProcess "stat" ["-c", "'%y'", fp] ""
+
+-- | last modification date of the file targeted by an item
+mdate
+  :: Item a
+  -> Compiler String
+mdate item = unsafeCompiler $ do
+  let fp = toFilePath $ itemIdentifier item
+  Process.readProcess "stat" ["-c", "'%y'", fp] ""
+
+-- | sort items by last modification date
+sortByMdate :: [Item a] -> Compiler [Item a]
+sortByMdate = sortByM mdate
+  where
+    sortByM :: (Monad m, Ord k) => (a -> m k) -> [a] -> m [a]
+    sortByM f xs = liftM (map fst . sortBy (comparing snd)) $
+                   mapM (\x -> liftM (x,) (f x)) xs
+
+-- | the basename of the target file
+basenameField :: String -> Context a
+basenameField key = field key $ \item -> unsafeCompiler $
+  -- TODO upgrade hakyll to v4.12.5.0: Update type of fromFilePath to use FilePath instead of String
+  -- return $ basename $ toFilePath $ itemIdentifier item
+  return $ takeBaseName $ toFilePath $ itemIdentifier item
+
+
+-- | Lookup for an 'alt' description of the image.
+--
+--   The 'alt' description is read from a file with extension @.alt@.
+--   The 'alt' file must be located in the same directory as the
+--   target image, and must have the same filename except for the
+--   extension name.
+--   TODO If no file is found, return TODO.
+--
+--   For example, for an image file in @foo/image.jpeg@, the alt
+--   description is read in file @foo/image.alt@.
+--
+--   In order to use this context in $if$ template constructs, we
+--   actually generate two contexts: a field @key ++ "-description"@
+--   that contains the alt description, and a field @key@ that is
+--   actually a 'boolField' to be used in @$if$@ template constructs.
+--
+-- MAYBE the function is evaluated *each time* the field value is read.
+imageAlt :: String -> Context a
+imageAlt key = field key $ \item -> do
+  let image = toFilePath $ itemIdentifier item
+      -- the 'alt' file
+      altFile = replaceExtension image "alt"
+  mres <- unsafeCompiler $
+    doesFileExist altFile >>= \case
+      True ->  Just <$> readFile altFile
+      False -> return Nothing
+  case mres of
+    -- we rely on the 'Compiler' instance of 'Alternative', so that we
+    -- can use the field in an $if$ construct.  Taken from
+    -- https://github.com/beerendlauwers/hakyll-extra/blob/cc4741a9781412108926ac2d7cf70f52a5ee68a3/src/Hakyll/Web/Template/Context/Extra.hs#L15
+    Just x  -> return x
+    Nothing -> empty
+
+-- | Append the basename of the target route with @addn@.  @addn@ is
+--   added before the last file extension.
+-- >>> runRoutes (appendFileRoute "small") "image.jpeg"
+-- "image.small.jpeg"
+-- >>> runRoutes (appendFileRoute "small") "foo/image.jpeg"
+-- "foo/image.small.jpeg"
+-- >>> runRoutes (appendFileRoute "small") "foo/bar/image.jpeg"
+-- "foo/bar/image.small.jpeg"
+appendFileRoute :: String -> Routes
+appendFileRoute addn = customRoute $
+  -- TODO upgrade hakyll to v4.12.5.0: Update type of fromFilePath to use FilePath instead of String
+  \i -> let f = toFilePath i
+        in dropExtension f <.> addn <.> takeExtension f
+
+
+-- * image processing
+--------------------------------------------------------------------------------
+
+type Width = Int
+type Height = Int
+
+-- | resize an image with imagemagick's binary @convert@.
+--   Assuming the current Identifier is an image.
+resizeImage :: Width -> Height -> Compiler (Item ())
+resizeImage w h = do
+  -- MAYBE.  check if input is an image.
+  input <- getUnderlying
+  output <- getRoute input >>= \case
+    Just o -> return o
+    Nothing -> fail $ "ERROR - route not found for" ++ show input
+  siteRoot <- destinationDirectory . compilerConfig <$> compilerAsk
+
+  unsafeCompiler $ do
+    let fullRoute = siteRoot </> makeRelative "/" output
+        resizeDims :: String
+        resizeDims = show w ++ "x" ++ show h
+
+    -- Hakyll's @makeDirectories@ does not work for our purpose: the
+    -- directory is not created (... is not created soon enough?)
+    createDirectoryIfMissing True $ takeDirectory fullRoute
+
+    _ <- Process.readProcess "convert" [toFilePath input, "-resize", resizeDims, "-quality", "91", fullRoute] ""
+    return $ Item input ()
